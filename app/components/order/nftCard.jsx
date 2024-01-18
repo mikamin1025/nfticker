49ad1eac7279eb11cd74dfcfa56ee3da188df5f2
Hitoshi Mikamie <mikamin1025@gmail.comm>
2024-01-19 00:16:43 +0900

    24.1.19作業終了時点

--- app/components/order/nftCard.jsx
+++ app/components/order/nftCard.jsx
@@ -5,10 +5,13 @@ export const NFTCard = ({ nft }) => {
     <div className={styles.card_block}>
       <img className={styles.card_img} src={nft.media[0].gateway}></img>
       <div className={styles.card_detail}>
-        <h2 className="">{nft.title}</h2>
-        <p className="">Id: {nft.id.tokenId}</p>
-        <p className="">{nft.contract.address}</p>
-        <p className="">{nft.description}</p>
+        <h2 className={styles.card_title}>{nft.title}</h2>
+        {/* <p className="">Id: {nft.id.tokenId}</p> */}
+        {/* <p className="">{nft.contract.address}</p> */}
+        {/* <p className={styles.card_description}>{nft.description}</p> */}
+        <button className={styles.addCart}>
+          Add To Cart
+        </button>
       </div>
     </div>
   );
--- app/order/page.jsx
+++ app/order/page.jsx
@@ -32,7 +32,7 @@ export default function Home() {
 
   return (
     <div className={styles.order_page}>
-      <div className="">
+      <div className={styles.order_page_header}>
         <MetaMask setWalletAddress={setWalletAddress} />
         <input
           onChange={(e) => {
@@ -51,12 +51,39 @@ export default function Home() {
           Let's go!
         </button>
       </div>
-      <div className={styles.order_nftcard}>
-        {NFTs.length
-          ? NFTs.map((nft) => {
-              return <NFTCard nft={nft}></NFTCard>;
-            })
-          : ""}
+
+      <div className={styles.order_page_body}>
+        <div className={styles.icon_cart}>
+          <div className={styles.icon_cart_inner}>
+            <svg
+              class="w-6 h-6 text-gray-800 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 18 20"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 15a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0h8m-8 0-1-4m9 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-9-4h10l2-7H3m2 7L3 4m0 0-.792-3H1"
+              />
+            </svg>
+            <span>0</span>
+          </div>
+        </div>
+
+        {/* NFTの画像表示（商品一覧） */}
+        <div className={styles.ListProduct}>
+          <div className={styles.order_nftcard}>
+            {NFTs.length
+              ? NFTs.map((nft) => {
+                  return <NFTCard nft={nft}></NFTCard>;
+                })
+              : ""}
+          </div>
+        </div>
       </div>
     </div>
   );

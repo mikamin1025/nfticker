2988cfcf7b5853443cf87a831f24079a606205e3
Hitoshi Mikamie <mikamin1025@gmail.comm>
2024-02-25 23:57:10 +0900

    デザイン大幅変更_240225

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,18 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
 import '../styles/reset.css'
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'NFT TO STICKER',
-  description: 'あなたの所有するNFTを現実世界でアピールしよう',
-}
+  title: "NFT TO STICKER",
+  description: "あなたの所有するNFTを現実世界でアピールしよう",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ja">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
